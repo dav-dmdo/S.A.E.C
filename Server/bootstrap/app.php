b408159f0bb890a03c67f4e9990a5b4c54a7852e
davdev <david.davila@correo.unimet.edu.ve>
2024-10-20 12:12:47 -0400

    feat(routes): :zap: added academic year router and routes

--- Server/bootstrap/app.php
+++ Server/bootstrap/app.php
@@ -3,18 +3,19 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
+        then: function () {
+            Route::prefix('api/v1/academic-year')
+                ->group(base_path('routes/academicYear.php'));
+        }
     )
-    ->withMiddleware(function (Middleware $middleware) {
-        $middleware->validateCsrfTokens(except: [
-            '/api/*',
-        ]);
-    })
+    ->withMiddleware(function (Middleware $middleware) {})
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
---
+++ Server/routes/academicYear.php
@@ -0,0 +1,8 @@
+<?php
+
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return 'welcome academic year';
+});
