641042d4d5306627750256b34c7b25f770be0b6c
davdev <david.davila@correo.unimet.edu.ve>
2024-10-27 15:19:43 -0400

    feat(Migrations): :construction: Added Program table as degrees_subjects table. Just migration, not model
    \

---
+++ Server/database/migrations/2024_10_27_185529_create_degrees_subjects_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('degrees_subjects', function (Blueprint $table) {
+            $table->id();
+            $table->string('degree_id');
+            $table->string('subject_id');
+
+            $table->foreign('degree_id')->references('degree_id')->on('degrees');
+            $table->foreign('subject_id')->references('subject_id')->on('subject');
+
+            $table->unique(['degree_id', 'subject_id'], 'unique_degree_subject_combination');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('degrees_subjects');
+    }
+};
