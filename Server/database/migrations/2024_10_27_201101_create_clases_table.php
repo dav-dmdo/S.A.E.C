24fd810f96793cfa654221ff34a0b2972e988fc5
davdev <david.davila@correo.unimet.edu.ve>
2024-10-27 16:24:47 -0400

    feat(Migrations): :sparkles: Added Clases migration and model

---
+++ Server/database/migrations/2024_10_27_201101_create_clases_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('clases', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('section_id');
+            $table->integer('clase_number');
+            $table->date('clase_date');
+            $table->boolean('clase_is_canceled');
+            $table->enum('clase_type', ['Evaluation', 'Class']);
+            $table->string('clase_topic');
+
+            $table->unique(['section_id','clase_number'],'unique_clase_combination');
+
+            $table->foreign('section_id')->references('id')->on('sections');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('clases');
+    }
+};
