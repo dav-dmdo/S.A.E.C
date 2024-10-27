43d0b3fcb2be5c677738378a25e4f594557e9991
davdev <david.davila@correo.unimet.edu.ve>
2024-10-27 15:56:05 -0400

    fix(Seeders): :bug: seeders modified because of changes made to tables

--- Server/database/seeders/AcademicYearSeeder.php
+++ Server/database/seeders/AcademicYearSeeder.php
@@ -19,21 +19,18 @@ class AcademicYearSeeder extends Seeder
         $academicYear->academic_year_id = 2122;
         $academicYear->academic_year_start_date = Carbon::create(2021, 9, 1)->format('d-m-y'); // Fecha: 1 de septiembre de 2021
         $academicYear->academic_year_end_date = Carbon::create(2022, 6, 30)->format('d-m-y');  // Fecha: 30 de junio de 2022
-        $academicYear->academic_year_description = 'Año académico 2021-2022';
         $academicYear->save();
 
         $academicYear = new AcademicYear();
         $academicYear->academic_year_id = 2223;
         $academicYear->academic_year_start_date = Carbon::create(2022, 9, 1)->format('d-m-y'); // Fecha: 1 de septiembre de 2022
         $academicYear->academic_year_end_date = Carbon::create(2023, 6, 30)->format('d-m-y'); // Fecha: 30 de junio de 2023
-        $academicYear->academic_year_description = 'Año académico 2022-2023';
         $academicYear->save();
 
         $academicYear = new AcademicYear();
         $academicYear->academic_year_id = 2324;
         $academicYear->academic_year_start_date = Carbon::create(2023, 9, 1)->format('d-m-y'); // Fecha: 1 de septiembre de 2023
         $academicYear->academic_year_end_date = Carbon::create(2024, 6, 30)->format('d-m-y'); // Fecha: 30 de junio de 2024
-        $academicYear->academic_year_description = 'Año académico 2023-2024';
         $academicYear->save();
 
 
--- Server/database/seeders/TermSeeder.php
+++ Server/database/seeders/TermSeeder.php
@@ -16,7 +16,6 @@ class TermSeeder extends Seeder
         $term1 = new Term();
         $term1->academic_year_id = 2122;
         $term1->term_id = 1;
-        $term1->term_name = 'First Term';
         $term1->term_start_date = '2021-09-01';
         $term1->term_end_date = '2021-12-22';
         $term1->term_type = 'First Term';
@@ -25,7 +24,6 @@ class TermSeeder extends Seeder
         $term2 = new Term();
         $term2->academic_year_id = 2122;
         $term2->term_id = 2;
-        $term2->term_name = 'Second Term';
         $term2->term_start_date = '2022-01-10';
         $term2->term_end_date = '2022-03-31';
         $term2->term_type = 'Second Term';
@@ -34,7 +32,6 @@ class TermSeeder extends Seeder
         $term3 = new Term();
         $term3->academic_year_id = 2122;
         $term3->term_id = 3;
-        $term3->term_name = 'Third Term';
         $term3->term_start_date = '2022-04-10';
         $term3->term_end_date = '2022-06-30';
         $term3->term_type = 'Third Term';
@@ -43,7 +40,6 @@ class TermSeeder extends Seeder
         $term1 = new Term();
         $term1->academic_year_id = 2223;
         $term1->term_id = 1;
-        $term1->term_name = 'First Term';
         $term1->term_start_date = '2022-09-01';
         $term1->term_end_date = '2022-12-22';
         $term1->term_type = 'First Term';
@@ -52,7 +48,6 @@ class TermSeeder extends Seeder
         $term2 = new Term();
         $term2->academic_year_id = 2223;
         $term2->term_id = 2;
-        $term2->term_name = 'Second Term';
         $term2->term_start_date = '2023-01-10';
         $term2->term_end_date = '2023-03-31';
         $term2->term_type = 'Second Term';
@@ -61,7 +56,6 @@ class TermSeeder extends Seeder
         $term3 = new Term();
         $term3->academic_year_id = 2223;
         $term3->term_id = 3;
-        $term3->term_name = 'Third Term';
         $term3->term_start_date = '2023-04-10';
         $term3->term_end_date = '2023-06-30';
         $term3->term_type = 'Third Term';
@@ -69,7 +63,6 @@ class TermSeeder extends Seeder
         $term1 = new Term();
         $term1->academic_year_id = 2324;
         $term1->term_id = 1;
-        $term1->term_name = 'First Term';
         $term1->term_start_date = '2023-09-01';
         $term1->term_end_date = '2023-12-22';
         $term1->term_type = 'First Term';
@@ -78,7 +71,6 @@ class TermSeeder extends Seeder
         $term2 = new Term();
         $term2->academic_year_id = 2324;
         $term2->term_id = 2;
-        $term2->term_name = 'Second Term';
         $term2->term_start_date = '2024-01-10';
         $term2->term_end_date = '2024-03-31';
         $term2->term_type = 'Second Term';
@@ -87,7 +79,6 @@ class TermSeeder extends Seeder
         $term3 = new Term();
         $term3->academic_year_id = 2324;
         $term3->term_id = 3;
-        $term3->term_name = 'Third Term';
         $term3->term_start_date = '2024-04-10';
         $term3->term_end_date = '2024-06-30';
         $term3->term_type = 'Third Term';
