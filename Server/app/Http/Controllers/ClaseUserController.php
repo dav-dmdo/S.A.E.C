ab0665c1254313458bf796763211891c33144d8c
Daniel De Oliveira <daniel.deoliveira@correo.unimet.edu.ve>
2024-11-04 00:35:55 -0400

    feat(controllers): ClaseUser (attendance) structure was created!

--- Server/app/Http/Controllers/ClaseUserController.php
+++ Server/app/Http/Controllers/ClaseUserController.php
@@ -6,5 +6,23 @@ use Illuminate\Http\Request;
 
 class ClaseUserController extends Controller
 {
-    //
+    public function index(){
+
+    }
+
+    public function show($id){
+
+    }
+
+    public function store(Request $request){
+
+    }
+
+    public function update(Request $request, $id){
+
+    }
+
+    public function destroy($user_ci){
+
+    }
 }
