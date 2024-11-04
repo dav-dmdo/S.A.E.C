e1ed8d0589c0c4ad83512edfb65bb652609e58d5
Daniel De Oliveira <daniel.deoliveira@correo.unimet.edu.ve>
2024-11-04 00:35:21 -0400

    changes(routes): API routes was defined!

--- Server/bootstrap/app.php
+++ Server/bootstrap/app.php
@@ -8,14 +8,9 @@ use Illuminate\Support\Facades\Route;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__ . '/../routes/web.php',
-        api: __DIR__ . '/../routes/api.php',
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
         then: function () {
-            Route::prefix('api/v1/academic-year')
-                ->group(base_path('routes/academicYear.php'));
-            Route::prefix('api/v1/term')
-                ->group(base_path('routes/term.php'));
             Route::prefix('api/v1/test')
                 ->group(base_path('routes/test.php'));
         }
--- Server/routes/web.php
+++ Server/routes/web.php
@@ -6,14 +6,21 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return "";
 });
 
 // User API
-Route::apiResource('api/user', UserController::class)
-    ->parameters([
-        "user" => "user_ci"
-    ]);
+Route::get('api/user', [UserController::class, 'index']);
+Route::post('api/user', [UserController::class, 'store']);
+Route::post('/api/user/login', [UserController::class, 'login']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/api/user/logout', [UserController::class, 'logout']);
+    // Show: Route to see details of current user
+    // Update: Route to update information
+    // Destroy: Route to delete the account
+});
 
 // Attendance API
-Route::apiResource('api/attendance', ClaseUserController::class);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('api/attendance', ClaseUserController::class);
+});
