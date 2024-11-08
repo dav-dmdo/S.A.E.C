d18659eeabd8ca935aec3d7ef92ac98dc36e4b47
Daniel De Oliveira <daniel.deoliveira@correo.unimet.edu.ve>
2024-11-08 15:30:54 -0400

    changes(teacher)

--- Server/database/seeders/TeacherSeeder.php
+++ Server/database/seeders/TeacherSeeder.php
@@ -15,9 +15,51 @@ class TeacherSeeder extends Seeder
     public function run(): void
     {
         Teacher::create([
-            "user_ci" => 10000000,
+            "user_ci" => 14567890,
             "teacher_card_id" => 20121110230,
             "teacher_hire_date" => Carbon::create(2012, 9, 1)->format('d-m-y'),
+        ]);
+        
+        Teacher::create([
+            "user_ci" => 17894523,
+            "teacher_card_id" => 20130910865,
+            "teacher_hire_date" => Carbon::create(2013, 9, 1)->format('d-m-y'),
+        ]);
+        
+        Teacher::create([
+            "user_ci" => 15678934,
+            "teacher_card_id" => 20140710456,
+            "teacher_hire_date" => Carbon::create(2014, 7, 1)->format('d-m-y'),
+        ]);
+        
+        Teacher::create([
+            "user_ci" => 12345678,
+            "teacher_card_id" => 20150910214,
+            "teacher_hire_date" => Carbon::create(2015, 9, 1)->format('d-m-y'),
+        ]);
+        
+        Teacher::create([
+            "user_ci" => 16543210,
+            "teacher_card_id" => 20160810879,
+            "teacher_hire_date" => Carbon::create(2016, 8, 1)->format('d-m-y'),
+        ]);
+        
+        Teacher::create([
+            "user_ci" => 14567832,
+            "teacher_card_id" => 20170910567,
+            "teacher_hire_date" => Carbon::create(2017, 9, 1)->format('d-m-y'),
+        ]);
+        
+        Teacher::create([
+            "user_ci" => 19876543,
+            "teacher_card_id" => 20180910456,
+            "teacher_hire_date" => Carbon::create(2018, 9, 1)->format('d-m-y'),
+        ]);
+        
+        Teacher::create([
+            "user_ci" => 10000000,
+            "teacher_card_id" => 20100910987,
+            "teacher_hire_date" => Carbon::create(2010, 9, 1)->format('d-m-y'),
         ]);
     }
 }
